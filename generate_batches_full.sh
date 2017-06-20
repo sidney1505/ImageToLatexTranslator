03f8a38effde9148e1ce237e557a0123d2653948
sidney1505 <usdlw@student.kit.edu>
2017-06-20 14:22:15 +0200

    delete duplicates

--- generate_batches_full.sh
+++
@@ -1,6 +0,0 @@
-python generate_batches.py \
---image-dir ../im2markup/data/fullPreprocessed/formula_images_processed \
---data-path ../im2markup/data/fullPreprocessed/im2latex_train_filter.lst \
---output-path ../im2markup/data/fullPreprocessed/im2latex_train_batches \
---label-path ../im2markup/data/fullPreprocessed/im2latex_formulas.norm.lst \
---vocabulary-path ../im2markup/data/fullPreprocessed/im2latex_latex_vocab.txt
